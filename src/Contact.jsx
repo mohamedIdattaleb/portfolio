fc5344686784025367153a5bbf4e41915ea53949
Mohamedtwd <superraja2003@gmail.com>
2025-06-26 13:58:48 +0100

    push

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -4,8 +4,8 @@ export default function Contact() {
             <h2>Contact</h2>
             <p>📧 Email : <a href="mailto:idattalebmohamed@gmail.com">idattalebmohamed@gmail.com</a></p>
             <p>📞 Téléphone : <a href="tel:+212762039684">+212 7 62 03 96 84</a></p>
-            <p>📸 Instagram : <a href="https://www.instagram.com/idattaleb_mohamed/" target="_blank">instagram.com/idattaleb_mohamed</a></p>
-            <p>💻 GitHub : <a href="https://github.com/mohamedidattaleb" target="_blank">github.com/mohamedidattaleb</a></p>
+            <p>📸 Instagram : <a href="https://www.instagram.com/idattaleb_mohamed/" target="_blank" rel="noreferrer">instagram.com/idattaleb_mohamed</a></p>
+            <p>💻 GitHub : <a href="https://github.com/mohamedidattaleb" target="_blank" rel="noreferrer">github.com/mohamedidattaleb</a></p>
         </section>
     );
 }
